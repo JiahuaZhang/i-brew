c4ba24803a13a1303795a6fb8a59e03e3b07196c
Jiahua Zhang <JiaHua_Zhang@comcast.com>
2021-05-20 00:26:01 -0400

    amplify backup

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,8 +7,13 @@ const options: NextAuthOptions = {
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Providers.Cognito({
+      clientId: process.env.COGNITO_CLIENT_ID,
+      domain: process.env.COGNITO_DOMAIN
     })
   ]
 }
 
+// Not used
 export default (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, options);
